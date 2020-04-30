73abe74705577a22904f92dde6ba4fdec1fb218a
Yuito Murase <yuito@acupof.coffee>
2020-04-30 23:01:30 +0900

    Pass test when snapshot matches

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,11 +1,29 @@
 /* eslint-disable global-require */
 
 describe('toMatchPdfSnapshot', () => {
+  const mockDiffPdfToSnapshot = jest.fn();
+  jest.mock('../src/diff-snapshot', () => ({
+    runDiffImageToSnapshot: mockDiffPdfToSnapshot,
+  }));
+
   it('should throw an error if used with .not matcher', () => {
     const { toMatchPdfSnapshot } = require('../src/index');
     expect.extend({ toMatchPdfSnapshot });
 
     expect(() => expect('path to pdf file').not.toMatchPdfSnapshot())
       .toThrowErrorMatchingSnapshot();
+  });
+
+  it('should pass when snapshot is same as baseline snapshot', () => {
+    mockDiffPdfToSnapshot.mockReturnValue({
+      pass: true,
+      diffOutputPath: 'path/to/result.png',
+    });
+
+    const { toMatchPdfSnapshot } = require('../src/index');
+    expect.extend({ toMatchPdfSnapshot });
+
+    expect(() => expect('pretendthisisanimagebuffer').toMatchPdfSnapshot())
+      .not.toThrow();
   });
 });
--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ function toMatchPdfSnapshot(received) {
   // eslint-disable-next-line no-console
   console.log(received);
 
-  throw new Error('Jest: `.not` cannot be used with `.toMatchPdfSnapshot()`.');
+  const { isNot } = this;
+  if (isNot) {
+    throw new Error('Jest: `.not` cannot be used with `.toMatchPdfSnapshot()`.');
+  }
+
+  return {
+    pass: true,
+    message: 'some message',
+  };
 }
 
 module.exports = {
